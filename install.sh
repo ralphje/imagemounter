d62bedcb63c84d549fcc64b70d844de204245b6b
Peter Wagenaar <sakartu@gmail.com>
2013-01-07 06:33:01 -0800

    Finished installscript

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@ if [[ $EUID -ne 0 ]]; then
 	echo This script needs to be ran as root!
 	exit 1
 fi
+/usr/bin/apt-get install uuid-dev python-dev libtsk-dev g++ build-essential
 
-tar xvf ./pytsk-2012-11-10.tgz
+/bin/tar xvf ./pytsk-2012-11-10.tgz
+
+cd ./pytsk
+/usr/bin/python ./setup.py build
+/usr/bin/python ./setup.py install
+cd ..
+echo All dependencies installed, you can now remove the pytsk folder
