1756f45232ba3d316f9a50403315a335b1474091
Peter Wagenaar <sakartu@gmail.com>
2013-03-28 05:52:22 -0700

    Add infinite unmount loop

--- util.py
+++ util.py
@@ -4,7 +4,6 @@ import logging
 import re
 import glob
 import os
-import pdb
 
 
 def unmount(cmd, mountpoint, tries=20):
@@ -13,7 +12,17 @@ def unmount(cmd, mountpoint, tries=20):
     command, giving it a couple of tries if necessary
     '''
     cmd.append(mountpoint)
-    try_persistent(cmd, tries=tries)
+    while True:
+        try:
+            subprocess.check_call(cmd)
+            break
+        except:
+            try:
+                print u'There are still files open in the mountpoint! Press [enter] to try again. If you want to skip unmounting, press ^C!'
+                raw_input('>>>')
+            except KeyboardInterrupt:
+                break
+
     for _ in range(tries):
         if os.listdir(mountpoint) == []:
             # Unmount was successful, remove mountpoint
@@ -28,25 +37,6 @@ def unmount(cmd, mountpoint, tries=20):
     return False
 
 
-def try_persistent(cmd, tries=20, wait=1):
-    '''
-    A subprocess helper method that will try a given command a few times, each
-    with a given wait period between the commands and starting with one wait
-    period. This helps with commands that require some time before they can be
-    handled successfully, such as unmounting images
-    '''
-    for i in range(tries):
-        try:
-            time.sleep(wait)
-            subprocess.check_call(cmd)
-            break
-        except subprocess.CalledProcessError:
-            pass
-        except OSError:
-            logging.exception('Error while calling external program:')
-            pdb.set_trace()
-
-
 def is_encase(path):
     return re.match(r'^.*\.E\w\w$', path)
 
@@ -56,7 +46,7 @@ def expand_path(path):
     Expand the given path to either an Encase image or a dd image
     i.e. if path is '/path/to/image.E01' then the result of this method will be
     /path/to/image.E*'
-    and if path is '/path/to/image.001' then the result of this method will be 
+    and if path is '/path/to/image.001' then the result of this method will be
     '/path/to/image.[0-9][0-9]?'
     '''
     if is_encase(path):
