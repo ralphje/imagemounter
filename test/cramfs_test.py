bff6e64e44e55e9667c080890b0ae8c4d0e94a0d
Martin van Wingerden <martinvw@gmail.com>
2015-03-20 20:30:49 +0100

    Further setup of test cases, also included an mbr based filesystem. We should later on include E01's as well. Note that the cramfs and minix fail at this moment. And some assertTrue's should still be replaced by assertEquals

---
+++ test/cramfs_test.py
@@ -0,0 +1,13 @@
+from basetest import BaseTestFilesystemMount
+
+class CramFSTest(BaseTestFilesystemMount):
+
+    def setUp(self):
+        self.filename = 'images/test.cramfs' 
+
+    def validate_count(self, volumes):
+        self.assertTrue(len(volumes) == 1)
+
+    def validate_types(self, volumes):
+        self.assertEquals(volumes[0].fstype, "cramfs")
+
