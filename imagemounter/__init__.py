fb4c1bd8e6787ef48ed7cbeef22b5a619d315d12
Ralph Broenink <ralph@ralphbroenink.net>
2015-06-17 09:11:04 +0200

    OK, this is actually going to be a 2.0 branch with all these backwards incompatible changes

--- imagemounter/__init__.py
+++ imagemounter/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 __ALL__ = ['Volume', 'Disk', 'ImageParser']
-__version__ = '1.6.0b2'
+__version__ = '2.0.0b3'
 
 BLOCK_SIZE = 512
 VOLUME_SYSTEM_TYPES = ('detect', 'dos', 'bsd', 'sun', 'mac', 'gpt', 'dbfiller')
