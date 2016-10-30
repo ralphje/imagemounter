70d9d799df1e5b68efcbb9bfa97b621de16c9017
Ralph Broenink <ralph@ralphbroenink.net>
2016-10-30 12:01:25 +0100

    Further improvements to the imount shell

--- imagemounter/cli/shell.py
+++ imagemounter/cli/shell.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import argparse
 import cmd
 import logging
 import os
 import shlex
 
+import pickle
+
 from imagemounter import __version__, DISK_MOUNTERS
 from imagemounter.cli import CheckAction, ImageMounterFormatter, get_coloring_func
 from imagemounter.disk import Disk
 from imagemounter.parser import ImageParser
+
+
+SAVE_PATH = os.path.expanduser("~/.imountshell")
 
 
 class ShellArgumentParser(argparse.ArgumentParser):
@@ -107,14 +114,55 @@ class ImageMounterShell(ArgumentParsedShell):
     prompt = '(imount) '
     file = None
     parser = None
+    args = None
+    saved = False
+
+    def save(self):
+        with open(SAVE_PATH, 'wb') as f:
+            pickle.dump(self.parser, f)
+        self.saved = True
+
+    def load(self):
+        with open(SAVE_PATH, 'rb') as f:
+            self.parser = pickle.load(f)
 
     def preloop(self):
-        self.stdout.write("Welcome to imagemounter {version}".format(version=__version__))
-        self.stdout.write("\n")
-        self.parser = ImageParser()
+        """if the parser is not already set, loads the parser."""
+        if not self.parser:
+            self.stdout.write("Welcome to imagemounter {version}".format(version=__version__))
+            self.stdout.write("\n")
+
+            self.parser = ImageParser()
+            for p in self.args.paths:
+                self.onecmd('disk "{}"'.format(p))
+
+    def onecmd(self, line):
+        """Do not crash the entire program when a single command fails."""
+        try:
+            return cmd.Cmd.onecmd(self, line)
+        except Exception as e:
+            print("Critical error.", e)
 
     def error(self, error):
+        """Writes an error to the console"""
         self.stdout.write('*** %s\n' % error)
+
+    def _get_all_indexes(self):
+        """Returns all indexes available in the parser"""
+        if self.parser:
+            return [v.index for v in self.parser.get_volumes()] + [d.index for d in self.parser.disks]
+        else:
+            return None
+
+    def _get_by_index(self, index):
+        """Returns a volume,disk tuple for the specified index"""
+        volume_or_disk = self.parser.get_by_index(index)
+        volume, disk = (volume_or_disk, None) if not isinstance(volume_or_disk, Disk) else (None, volume_or_disk)
+        return volume, disk
+
+    ######################################################################
+    # disk command
+    ######################################################################
 
     def parser_disk(self, parser):
         parser.description = "Add a disk to the current parser"
@@ -135,11 +183,9 @@ class ImageMounterShell(ArgumentParsedShell):
             pass
         print("Added {path} to the image mounter as index {index}".format(path=args.path, index=disk.index))
 
-    def _get_all_indexes(self):
-        if self.parser:
-            return [v.index for v in self.parser.get_volumes()] + [d.index for d in self.parser.disks]
-        else:
-            return None
+    ######################################################################
+    # mount command
+    ######################################################################
 
     def parser_mount(self, parser):
         parser.description = "Mount a volume by its index"
@@ -148,30 +194,40 @@ class ImageMounterShell(ArgumentParsedShell):
 
     def arg_mount(self, args):
         col = get_coloring_func()
-        volume_or_disk = self.parser.get_by_index(args.index)
-        volume, disk = (volume_or_disk, None) if not isinstance(volume_or_disk, Disk) else (None, volume_or_disk)
+        volume, disk = self._get_by_index(args.index)
 
         if not args.recursive:
-            if disk and not disk.is_mounted:
-                try:
-                    disk.mount()
-                except Exception as e:
-                    pass
+            if disk:
+                if not disk.is_mounted:
+                    try:
+                        disk.mount()
+                    except Exception as e:
+                        pass
+                else:
+                    print(col("Disk {} is already mounted.".format(disk.index), 'red'))
             else:
-                try:
-                    volume.init_volume()
+                if not volume.is_mounted:
+                    try:
+                        volume.init_volume()
+                        if volume.is_mounted:
+                            if volume.mountpoint:
+                                print("Mounted volume {index} at {path}"
+                                      .format(path=col(volume.mountpoint, "green", attrs=['bold']),
+                                              index=volume.index))
+                            else:
+                                print("Mounted volume {index} (no mountpoint available)".format(index=volume.index))
+                        else:
+                            print("Refused to mount volume {index}.".format(index=volume.index))
+                    except Exception as e:
+                        print(col("An error occurred while mounting volume {index}: {type}: {args}"
+                                  .format(type=type(e).__name__,
+                                          args=" ".join(map(str, e.args)),
+                                          index=volume.index), "red"))
+                else:
                     if volume.mountpoint:
-                        print("Mounted volume {index} at {path}"
-                              .format(path=col(volume.mountpoint, "green", attrs=['bold']),
-                                      index=volume.index))
+                        print(col("Volume {} is already mounted at {}.".format(volume.index, volume.mountpoint), 'red'))
                     else:
-                        print("Mounted volume {index} (no mountpoint available)".format(index=volume.index))
-                except Exception as e:
-                    import traceback ; traceback.print_exc()
-                    print(col("An error occurred while mounting volume {index}: {type}: {args}"
-                              .format(type=type(e).__name__,
-                                      args=" ".join(map(str, e.args)),
-                                      index=volume.index), "red"))
+                        print(col("Volume {} is already mounted.".format(volume.index), 'red'))
         else:
             if disk:
                 it = disk.init_volumes
@@ -189,8 +245,12 @@ class ImageMounterShell(ArgumentParsedShell):
                                       args=" ".join(map(str, e.args)),
                                       index=v.index), "red"))
 
+    ######################################################################
+    # unmount command
+    ######################################################################
+
     def parser_unmount(self, parser):
-        parser.description = "Unmount a volume by its index"
+        parser.description = "Unmount a disk or volume by its index. Is recursive by design."
         parser.add_argument('index', help='volume index', nargs='?', choices=self._get_all_indexes())
 
     def arg_unmount(self, args):
@@ -202,13 +262,74 @@ class ImageMounterShell(ArgumentParsedShell):
             self.parser.clean()
             print("Unmounted everything")
 
-    def do_show(self, args):
+    ######################################################################
+    # show command
+    ######################################################################
+
+    def parser_show(self, parser):
+        parser.description = "Without arguments, displays a tree view of all known volumes and disks. With an " \
+                             "argument, it provides details about the referenced volume or disk."
+        parser.add_argument('index', help='volume index', nargs='?', choices=self._get_all_indexes())
+
+    def arg_show(self, args):
+        if not args.index:
+            self._show_tree()
+        else:
+            col = get_coloring_func()
+            volume, disk = self._get_by_index(args.index)
+
+            # displays a volume's details
+            if volume:
+                print(col("Details of volume {description}".format(description=volume.get_description()),
+                          attrs=['bold']))
+                if volume.is_mounted:
+                    print(col("Currently mounted", 'green'))
+
+                print("\nAttributes:")
+                for attr in ['size', 'offset', 'slot', 'flag', 'block_size', 'fstype', 'key']:
+                    print(" - {name:<15} {value}".format(name=attr, value=getattr(volume, attr)))
+
+                if volume.mountpoint or volume.loopback or volume._paths:
+                    print("\nPaths:")
+                    if volume.mountpoint:
+                        print(" - mountpoint      {value}".format(value=volume.mountpoint))
+                    if volume.loopback:
+                        print(" - loopback        {value}".format(value=volume.loopback))
+                    for k, v in volume._paths.items():
+                        print(" - {name:<15} {value}".format(name=k, value=v))
+
+                if volume.info:
+                    print("\nAdditional information:")
+                    for k, v in volume.info.items():
+                        print(" - {name:<15} {value}".format(name=k, value=v))
+
+            else:  # displays a disk's details
+                print(col("Details of disk {description}".format(description=disk.paths[0]), attrs=['bold']))
+                if disk.is_mounted:
+                    print(col("Currently mounted", 'green'))
+
+                print("\nAttributes:")
+                for attr in ['offset', 'block_size', 'read_write', 'disk_mounter']:
+                    print(" - {name:<15} {value}".format(name=attr, value=getattr(disk, attr)))
+
+                if disk.mountpoint or disk.rwpath or disk._paths:
+                    print("\nPaths:")
+                    if disk.mountpoint:
+                        print(" - mountpoint      {value}".format(value=disk.mountpoint))
+                    if disk.rwpath:
+                        print(" - rwpath          {value}".format(value=disk.rwpath))
+                    for k, v in disk._paths.items():
+                        print(" - {name:<15} {value}".format(name=k, value=v))
+
+    def _show_tree(self):
         col = get_coloring_func()
+
         for disk in self.parser.disks:
-            print("- {index:<5}  {type} {filename}"
-                  .format(index=col("{:<5}".format(disk.index), 'green' if disk.is_mounted else None, attrs=['bold']),
-                          type=col("{:<10}".format(disk.volumes.vstype), attrs=['dark']),
-                          filename=disk.paths[0]))
+            print("- {index:<5}  {type} {filename}".format(
+                index=col("{:<5}".format(disk.index), 'green' if disk.is_mounted else None, attrs=['bold']),
+                type=col("{:<10}".format(disk.volumes.vstype), attrs=['dark']),
+                filename=disk.paths[0]
+            ))
 
             def _show_volume_system(volumes, level=0):
                 level += 1
@@ -216,18 +337,34 @@ class ImageMounterShell(ArgumentParsedShell):
                     level_str = "  "*level + ("└ " if i == len(volumes)-1 else "├ ")
                     tp = v.volumes.vstype if v.fstype == 'volumesystem' else v.fstype if v.flag == 'alloc' else v.flag
 
-                    print("{level_str}{index}  {type} {size:<10}  {description}"
-                          .format(level_str=level_str,
-                                  index=col("{:<5}".format(v.index), 'green' if v.is_mounted else None, attrs=['bold']),
-                                  type=col("{:<10}".format(tp), attrs=['dark']),
-                                  description=v.get_description(with_index=False, with_size=False)[:30],
-                                  size=v.get_formatted_size()))
+                    print("{level_str}{index}  {type} {size:<10}  {description}".format(
+                        level_str=level_str,
+                        index=col("{:<5}".format(v.index), 'green' if v.is_mounted else None, attrs=['bold']),
+                        type=col("{:<10}".format(tp), attrs=['dark']),
+                        description=v.get_description(with_index=False, with_size=False)[:30],
+                        size=v.get_formatted_size()
+                    ))
                     _show_volume_system(v.volumes, level)
 
             _show_volume_system(disk.volumes)
 
+    ######################################################################
+    # quit command
+    ######################################################################
+
+    def do_save(self, arg):
+        self.save()
+
+    def do_EOF(self, arg):
+        self.save()
+        return True
+
     def do_quit(self, arg):
         """Quits the program."""
+        if self.saved:
+            self.save()
+        else:
+            self.parser.clean()
         return True
 
 
@@ -237,6 +374,8 @@ def main():
     parser.add_argument('--check', action=CheckAction, nargs=0,
                         help='do a system check and list which tools are installed')
     parser.add_argument('-v', '--verbose', action='count', default=False, help='enable verbose output')
+    parser.add_argument('-i', '--interactive', action='store_true', default=False, help='ignored option')
+    parser.add_argument('paths', nargs='*', help='path(s) to the image(s) that you want to mount')
 
     args = parser.parse_args()
     col = get_coloring_func()
@@ -248,10 +387,23 @@ def main():
     logger.addHandler(handler)
 
     shell = ImageMounterShell()
-    try:
-        shell.cmdloop()
-    except KeyboardInterrupt:
-        pass
+    shell.args = args
+    if os.path.exists(SAVE_PATH):
+        try:
+            inp = raw_input
+        except NameError:
+            inp = input
+        if 'n' not in inp('>>> Do wish to continue your previous session? [Y/n] ').lower():
+            shell.load()
+        os.unlink(SAVE_PATH)
+
+    while True:
+        try:
+            shell.cmdloop()
+        except KeyboardInterrupt:
+            print("Please exit using the 'quit' command.")
+        else:
+            break
 
 
 if __name__ == '__main__':
