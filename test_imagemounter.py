b722d3033811fc666137edb0774bc46b79cf746c
Ralph Broenink <ralph@ralphbroenink.net>
2017-07-02 14:19:36 +0200

    Update test suite and Travis config

--- test_imagemounter.py
+++
@@ -1,6 +0,0 @@
-from test import *
-import unittest
-
-if __name__ == '__main__':
-    unittest.main()
-
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import logging
+
+logger = logging.getLogger("imagemounter")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
