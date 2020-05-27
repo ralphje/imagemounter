ef3a598bda47ebb21586f870874f751229f00ab9
Ralph Broenink <ralph@ralphbroenink.net>
2020-05-27 16:11:11 +0200

    Fix tests for python 3.5

--- tests/volume_system_test.py
+++ tests/volume_system_test.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 import unittest.mock as mock
 
@@ -7,12 +8,13 @@ from imagemounter.disk import Disk
 
 
 class PartedTest(unittest.TestCase):
+    @unittest.skipIf(sys.version_info < (3, 6), "This test uses assert_called() which is not present before Py3.6")
     @mock.patch("imagemounter.volume_system._util.check_output_")
     def test_parted_requests_input(self, check_output):
         def modified_command(cmd, *args, **kwargs):
             if cmd[0] == 'parted':
                 # A command that requests user input
-                return check_output_(["python2", "-c", "exec \"try: raw_input('>> ')\\nexcept: pass\""],
+                return check_output_([sys.executable, "-c", "exec(\"try: input('>> ')\\nexcept: pass\")"],
                                      *args, **kwargs)
             return mock.DEFAULT
         check_output.side_effect = modified_command
